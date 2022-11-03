7527676a1d1df1c129d0901841d03da6fac8bcc7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-11-03 18:13:52 +0600

    Api add

---
+++ src/ApiFetch.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState }  from "react";
+import axios from "axios";
+const ApiFetch =() =>{
+    const [post1, setPost] = useState([]);
+
+    useEffect(() =>{
+        axios.get("https://api.ambor.com.bd/ambor/product/nospecification_products/?page=1&size=2").then((response) =>{
+            setPost(response.data.results) 
+        })
+    },[] )
+
+    // console.log(post1);
+
+
+    return (
+        <div>
+         {
+            post1.map((item, i) =>{
+                console.log(i)
+                return (
+
+                    <div key={i}>
+                        <p>{item.brand}</p>
+                        <p>{item.category}</p>
+                    </div>
+                )
+            })
+         }
+        </div>
+    )
+}
+export default ApiFetch;
