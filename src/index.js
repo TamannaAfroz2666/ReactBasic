70ad7496ff3b77c4f4bb3c16e1435a3893888fff
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-11-03 09:49:27 +0600

    UseState Complete

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import  ReactDOM  from "react-dom";
 // import Student from "./Student"
 // import Team1 from './Team'
 // import Garage from './gourje';
-import ContainerFluidExample from './FromValidation';
+// import ContainerFluidExample from './FromValidation';
+import UseStateFunc from './UseState';
     
 
 
 // let el = <h1>Hello </h1>
 // ReactDOM.render(<App/>, document.getElementById("root"));
-ReactDOM.render(<ContainerFluidExample/>, document.getElementById("root"));
+// ReactDOM.render(<ContainerFluidExample/>, document.getElementById("root"));
+ReactDOM.render(< UseStateFunc />, document.getElementById("root"));
